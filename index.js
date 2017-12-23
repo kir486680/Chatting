45e309010de15a06e95c8c64f2399024b55b1597
kir486680 <kir486680@gmail.com>
2017-12-23 15:53:48 -0500

    Setup start script and heroku port

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var db = mongoose.connection;
  var app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({defaultLayout:'layout'}));
